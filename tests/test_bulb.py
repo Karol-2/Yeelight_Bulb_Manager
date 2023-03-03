1a69b69e04d6a86906583cac0069d1d913c7b2d8
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2023-03-03 13:53:50 +0100

    Tests to color and info functions

--- tests/test_bulb.py
+++ tests/test_bulb.py
@@ -2,8 +2,9 @@ import os
 import unittest
 from unittest.mock import patch
 
+from colour import hex2rgb
 from yeelight import *
-
+from io import StringIO
 from app.menu_functions import *
 
 
@@ -40,9 +41,9 @@ class TestBulbFunctions(unittest.TestCase):
         maximal = int(self.bulb.get_model_specs()["color_temp"]["max"])
         temp = int(self.bulb.get_properties()['ct'])
 
-        self.assertEqual(temp,2000)
-        self.assertLessEqual(temp,int(maximal))
-        self.assertGreaterEqual(temp,int(minimal))
+        self.assertEqual(temp, 2000)
+        self.assertLessEqual(temp, int(maximal))
+        self.assertGreaterEqual(temp, int(minimal))
 
     @patch('builtins.input', return_value='5')
     def test_temperature_too_small_value(self, mock_input):
@@ -55,3 +56,28 @@ class TestBulbFunctions(unittest.TestCase):
     @patch('builtins.input', return_value='bulb')
     def test_temperature_not_number(self, mock_input):
         self.assertEqual(temperature(self.bulb), "Wrong input!")
+
+    @patch('builtins.input', return_value='lavender')
+    def test_color_valid(self, mock_input):
+        lavender_rgb = (230, 230, 250)
+        color(self.bulb)
+        bulb_color_hex = '#' + hex(int(self.bulb.get_properties()['rgb']))[2::]
+        bulb_color_val = hex2rgb(bulb_color_hex)
+        bulb_color_rgb = tuple(x * 255 for x in bulb_color_val)
+
+        self.assertEqual(int(bulb_color_rgb[0]), lavender_rgb[0], "First rgb value incurrect!")
+        self.assertEqual(int(bulb_color_rgb[1]), lavender_rgb[1], "Second rgb value incurrect!")
+        self.assertEqual(int(bulb_color_rgb[2]), lavender_rgb[2], "Third rgb value incurrect!")
+
+    @patch('builtins.input', return_value='pinky yellow green grass')
+    def test_color_invalid_input(self, mock_input):
+        color(self.bulb)
+        self.assertEqual(color(self.bulb), "Wrong input!", "Accepted invalid input!")
+
+    def test_info_showcase(self):
+        phrases_to_find = ["power", "color_temp", "name"]
+        captured_output = StringIO()
+        info(self.bulb)
+        output = captured_output.getvalue()
+        for phrase in phrases_to_find:
+            self.assertIn(phrase, output)
